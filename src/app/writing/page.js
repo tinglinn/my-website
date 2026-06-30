e9f9f1b75c27d7b2f4243ec58bb0c549d1cbf1d6
ting <tinglin0708@gmail.com>
2026-06-29 17:12:55 -0700

    fix escape

--- src/app/writing/page.js
+++ src/app/writing/page.js
@@ -2,27 +2,25 @@
 
 import React, { Component } from "react";
 import '../globals.css';
-// import Link from 'next/link';
-// import Image from 'next/image'
 
 export default function Writing() {
     return (
         <div className="text">
             <h3>Poetry</h3>
             <ul>
-                <li>"The Happiest Place on Earth", "One Year", Michigan Quarterly Review, Forthcoming</li>
-                <li><a href="https://poetrysociety.org/poems/piano">"Piano"</a>, Poetry Society of America, June 2026</li>
-                <li><a href="https://www.washingtonsquarereview.com/ting-lin-55">"Intention"</a>, <a href="https://www.washingtonsquarereview.com/ting-lin-55">"Ending"</a>, Washington Square Review, Spring 2026</li>
-                <li>"Leaving", Hayden's Ferry Review, June 2026</li>
-                <li><a href="https://www.cincinnatireview.com/micros/biology/">"Biology"</a>, The Cincinnati Review, September 2025</li>
-                <li><a href="https://fourwayreview.com/the-foreign-journalist-did-not-have-to-write-anything-new-by-ting-lin/">"The Foreign Journalist Did Not Have to Write Anything New"</a>, Four Way Review, November 2024</li>
-                <li><a href="https://aaww.org/another-year/">"Another Year"</a>, The Margins, May 2024</li>
+                <li>&ldquo;The Happiest Place on Earth&rdquo;, &ldquo;One Year&rdquo;, Michigan Quarterly Review, Forthcoming</li>
+                <li><a href="https://poetrysociety.org/poems/piano">&ldquo;Piano&rdquo;</a>, Poetry Society of America, June 2026</li>
+                <li><a href="https://www.washingtonsquarereview.com/ting-lin-55">&ldquo;Intention&rdquo;</a>, <a href="https://www.washingtonsquarereview.com/ting-lin-55">&ldquo;Ending&rdquo;</a>, Washington Square Review, Spring 2026</li>
+                <li>&ldquo;Leaving&rdquo;, Hayden&apos;s Ferry Review, June 2026</li>
+                <li><a href="https://www.cincinnatireview.com/micros/biology/">&ldquo;Biology&rdquo;</a>, The Cincinnati Review, September 2025</li>
+                <li><a href="https://fourwayreview.com/the-foreign-journalist-did-not-have-to-write-anything-new-by-ting-lin/">&ldquo;The Foreign Journalist Did Not Have to Write Anything New&rdquo;</a>, Four Way Review, November 2024</li>
+                <li><a href="https://aaww.org/another-year/">&ldquo;Another Year&rdquo;</a>, The Margins, May 2024</li>
             </ul>
 
             <h3>Essays & Criticism</h3>
             <ul>
-                <li>Review of <a href="https://www.thenation.com/article/culture/zhang-yueran-women-seated/">Zhang Yueran's Women, Seated</a>, The Nation, March 2026.</li>
-                <li>Review of <a href="https://www.thenation.com/article/culture/shuang-xuetao-hunter/">Shuang Xuetao's Hunter</a>, The Nation, December 2025.</li>
+                <li>Review of <a href="https://www.thenation.com/article/culture/zhang-yueran-women-seated/">Zhang Yueran&apos;s Women, Seated</a>, The Nation, March 2026.</li>
+                <li>Review of <a href="https://www.thenation.com/article/culture/shuang-xuetao-hunter/">Shuang Xuetao&apos;s Hunter</a>, The Nation, December 2025.</li>
                 <li>On <a href="https://thebaffler.com/latest/queer-memories-in-beijing-lin-wang">the first queer bar in Beijing</a>, The Baffler, August 2021 (NLGJA Excellence in International Coverage 2022)</li>
                 <li>On <a href="https://www.sixthtone.com/news/1007762/remembering-those-who-returned">returning from gold mountain</a>, Sixth Tone, June 2021</li>
                 <li>On <a href="http://www.heichimagazine.org/en/articles/995/escaping-real-time-on-cantopop-nostalgia">Cantopop and being the wrong audience</a>, Heichi Magazine, October 2021.</li>
@@ -32,7 +30,7 @@ export default function Writing() {
 
             <h3>Reporting</h3>
             <ul>
-                <li>On <a href="https://radiichina.com/netease-leftist/">young Chinese leftists, Soviet music and revolutionary radios</a>, RADII, January 2021 </li>
+                <li>On <a href="https://radiichina.com/netease-leftist/">young Chinese leftists, Soviet music and revolutionary radios</a>, RADII, January 2021</li>
                 <li>On <a href="https://supchina.com/2020/12/01/evil-man-github-archive-of-gendered-violence-in-china-goes-viral-on-social-media/">a viral GitHub archive of gendered violence</a>, SupChina, December 2020</li>
                 <li>On <a href="https://supchina.com/2020/11/12/the-entire-sky-a-brief-history-of-chinas-lesbian-activism/">a history of lesbian activism</a>, SupChina, November 2020</li>
             </ul>
